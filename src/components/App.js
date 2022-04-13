838a16014958fca375cab9c2959cd5868abcacf4
X-DSI <xmandsi@gmail.com>
2022-04-13 07:01:15 +0530

    Basic Functionality Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
-  const contact = [
-    {
-      id: "1",
-      name: "Daniel",
-      email: "dsi@gmail.com",
-    },
-    {
-      id: "2",
-      name: "Syles",
-      email: "syles@gmail.com",
-    },
-  ];
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    console.log(contact);
+  };
+
   return (
     <div className="ui container">
       <Header />
-      <AddContact />
-      <ContactList />
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} />
     </div>
   );
 }
