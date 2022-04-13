5ca6469591fa0940b3dc8a7ad87ff8c70a109308
X-DSI <xmandsi@gmail.com>
2022-04-13 12:09:40 +0530

    Updates Routers

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import uuid from "react-native-uuid";
 import Header from "./Header";
@@ -32,9 +33,15 @@ function App() {
   }, [contacts]);
   return (
     <div className="ui container">
-      <Header />
-      <AddContact addContactHandler={addContactHandler} />
-      <ContactList contacts={contacts} getContactId={removeContactHandler} />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/add" component={AddContact} />
+          <Route path="/" component={ContactList} />
+        </Routes>
+        {/* <AddContact addContactHandler={addContactHandler} /> */}
+        {/* <ContactList contacts={contacts} getContactId={removeContactHandler} /> */}
+      </Router>
     </div>
   );
 }
