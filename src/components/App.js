ac552906935a53790583dafa127263b0e0496cab
X-DSI <xmandsi@gmail.com>
2022-05-19 13:51:48 +0530

    Update

--- src/components/App.js
+++ src/components/App.js
@@ -31,6 +31,7 @@ function App() {
   useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
   }, [contacts]);
+
   return (
     <div className="ui container">
       <Router>
